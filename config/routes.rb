0fca5f1e8f6a59fb7311ae638c0f7a466fe88e7a
ryo27-jp <calvee.20@gmail.com>
2021-04-28 16:28:01 +0900

    ログインログアウト

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'home#index'
   devise_for :users, controllers: {
-    registrations: 'users/registrations'
+    registrations: 'users/registrations',
     sessions: 'users/sessions'
   }
 end
