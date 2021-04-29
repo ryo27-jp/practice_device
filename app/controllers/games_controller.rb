237a6e3d1720bdac8aaee04e9c9f4763ae4029b8
ryo27-jp <calvee.20@gmail.com>
2021-04-29 16:19:27 +0900

    gamesの基本CRUD作成

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -17,12 +17,31 @@ class GamesController < ApplicationController
     if @game.save
       redirect_to games_path, notice: "#{@game.title}を作成しました。"
     else
-      flash.noe[:danger] = '投稿に失敗しました。'
+      flash.now[:danger] = '作成に失敗しました。'
       render :new
     end
   end
 
   def edit
+    @game = Game.find(params[:id])
+  end
+
+  def update
+    @game = Game.find(params[:id])
+
+    if @game.update(game_params)
+      redirect_to games_path, notice: "#{@game.title}を編集しました。"
+    else
+      flash.now[:danger] = '編集に失敗しました。'
+      render :edit
+    end
+  end
+
+  def destroy
+    @game = Game.find(params[:id])
+
+    @game.destroy!
+    redirect_to games_path, notice: "#{@game.title}を削除しました。"
   end
 
   private
