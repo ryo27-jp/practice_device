3ad74e14259e959d6313a464b242b64e202a329e
ryo27-jp <calvee.20@gmail.com>
2021-04-29 11:48:29 +0900

    bootstrap追加

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -12,8 +12,22 @@ class GamesController < ApplicationController
   end
 
   def create
+    @game = Game.new(game_params)
+
+    if @game.save
+      redirect_to games_path, notice: "#{@game.title}を作成しました。"
+    else
+      flash.noe[:danger] = '投稿に失敗しました。'
+      render :new
+    end
   end
 
   def edit
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:title, :text)
   end
 end
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,11 @@
 const { environment } = require('@rails/webpacker')
 
+const webpack = require('webpack')
+environment.plugins.prepend('Provides',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jquery: 'jquery'
+  })
+)
+
 module.exports = environment
