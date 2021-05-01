d6b999cfd19d90c8f1a2b219214d6e59c0bc1421
ryo27-jp <calvee.20@gmail.com>
2021-05-01 20:55:41 +0900

    rankingsコントローラー

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'rankings/index'
+  get 'rankings/show'
+  get 'rankings/new'
+  get 'rankings/edit'
   get 'mypage/show'
   root to: 'home#index'
   devise_for :users, controllers: {
