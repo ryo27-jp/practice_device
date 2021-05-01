6eb5b8506d92d72287bccd0287a2b31a404b3263
ryo27-jp <calvee.20@gmail.com>
2021-05-01 20:53:42 +0900

    rankingモデル作成

---
+++ db/migrate/20210501065630_create_rankings.rb
@@ -0,0 +1,12 @@
+class CreateRankings < ActiveRecord::Migration[6.1]
+  def change
+    create_table :rankings do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :title, null: false
+      t.references :first_place, foreign_key: {to_table: :games}
+      t.references :second_place, foreign_key: {to_table: :games}
+
+      t.timestamps
+    end
+  end
+end
