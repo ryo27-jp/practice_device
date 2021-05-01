ad85201c0bdff793d793fc7b70d5a437a2fc06f4
ryo27-jp <calvee.20@gmail.com>
2021-05-01 21:27:28 +0900

    ランキングとりあえず表示

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,5 +1,6 @@
 class RankingsController < ApplicationController
   def index
+    @rankings = Ranking.all
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
-  get 'rankings/index'
-  get 'rankings/show'
-  get 'rankings/new'
-  get 'rankings/edit'
   get 'mypage/show'
   root to: 'home#index'
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions'
   }
+  resources :rankings
   resources :games
 end
