66ba3d404a491cdf5e77c9d648250cd0a12eafa6
ryo27-jp <calvee.20@gmail.com>
2021-05-02 16:52:42 +0900

    rankingCRUD

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -7,8 +7,46 @@ class RankingsController < ApplicationController
   end
 
   def new
+    @ranking = Ranking.new
   end
 
+  def create
+    @ranking = current_user.rankings.new(ranking_params)
+
+    if @ranking.save
+      redirect_to rankings_path, notice: "ランキングを作成しました"
+    else
+      flash.now[:notice] = "作成に失敗しました"
+      render :new 
+    end
+  end
+
+
   def edit
+    @ranking = current_user.rankings.find(params[:id])
+  end
+
+  def update
+    @ranking = current_user.rankings.find(params[:id])
+
+    if @ranking.update(ranking_params)
+      redirect_to rankings_path, notice: "編集しました"
+    else
+      flash.now[:notice] = "失敗しました"
+      render :edit
+    end
+  end
+
+  def destroy
+    @ranking = current_user.rankings.find(params[:id])
+
+    @ranking.destroy!
+    redirect_to rankings_path, notice:"削除しました"
+  end
+  
+  private
+
+  def ranking_params
+    params.require(:ranking).permit(:title, :first_place_id, :second_place_id)
   end
 end
