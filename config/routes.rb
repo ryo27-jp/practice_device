856c353d9b74f8fac7e560e5c52aaa2b63a61211
ryo27-jp <calvee.20@gmail.com>
2021-05-14 22:24:36 +0900

    ルート追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   root to: 'home#index'
   devise_for :users, controllers: {
     registrations: 'users/registrations',
-    sessions: 'users/sessions'
+    sessions: 'users/sessions',
+    omniauth_callbacks: 'users/omniauth_callbacks'
   }
   resources :rankings
   resources :games do
